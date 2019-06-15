07b1aef7d42aaf5f7c69e615dc53180850db95a4
guina.costa <agcosta1991.android@gmail.com>
2019-06-15 13:36:20 +0000

    cognito app configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ADWTpYE0L', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2tthp12n1kkoada90hk94hmf1p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
