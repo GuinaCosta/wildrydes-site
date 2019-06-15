035eb30adbac5aae332b304c7e7eeb86132876ce
guina.costa <agcosta1991.android@gmail.com>
2019-06-15 14:32:32 +0000

    add api host configuration

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ADWTpYE0L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2tthp12n1kkoada90hk94hmf1p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_ADWTpYE0L',
+        userPoolClientId: '2tthp12n1kkoada90hk94hmf1p', 
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7rynq68kda.execute-api.us-east-1.amazonaws.com/prd' 
     }
 };
